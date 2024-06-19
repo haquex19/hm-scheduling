09439ed8dc5ec6003524cc760fc409627eb2bf18
Mahbubul Haque <mhaquex19@gmail.com>
2024-06-18 20:19:12 -0400

    Remove auth code that couldn't be completed in time.

--- src/Hm.Scheduling.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Hm.Scheduling.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Duende.IdentityServer.Models;
-using Hm.Scheduling.Core.Settings;
+﻿using Hm.Scheduling.Core.Settings;
 using Hm.Scheduling.Core.Stores;
 using Hm.Scheduling.Infrastructure.Database;
 using Hm.Scheduling.Infrastructure.Stores;
@@ -16,51 +15,10 @@ public static class ServiceCollectionExtensions
         services
             .AddSingleton(Options.Create(settings))
             .AddDbContext<HmDbContext>(options => options.UseNpgsql(settings.ConnectionString))
-            .AddAuth()
             .AddScoped<IAppointmentAvailabilityStore, AppointmentAvailabilityStore>()
             .AddScoped<IReservationStore, ReservationStore>()
             .AddScoped<IUserStore, UserStore>();
 
-        return services;
-    }
-
-    private static IServiceCollection AddAuth(this IServiceCollection services)
-    {
-        services
-            .AddIdentityServer()
-            .AddInMemoryApiScopes(
-                new List<ApiScope>
-                {
-                    new ApiScope("api1", "My API")
-                })
-            .AddInMemoryClients(
-                new List<Client>
-                {
-                    new Client
-                    {
-                        ClientId = "Client1",
-                        AllowedGrantTypes = GrantTypes.ClientCredentials,
-                        ClientSecrets = new List<Secret>
-                        {
-                            new Secret("MySecret".Sha256())
-                        },
-                        AllowedScopes = new List<string>
-                        {
-                            "api1"
-                        }
-                    }
-                });
-
-        services
-            .AddAuthentication()
-            .AddJwtBearer(
-                options =>
-                {
-                    options.Authority = "https://localhost:7096";
-                    options.TokenValidationParameters.ValidateAudience = false;
-                });
-
-        services.AddAuthorization();
         return services;
     }
 }
